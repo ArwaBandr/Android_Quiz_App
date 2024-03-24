f51ee04ad4b9a56ba1a5288cbadc917d24305098
asus <arwa.ba00@gmail.com>
2024-03-25 00:45:28 +0300

    add switch compose

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -7,25 +7,36 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.quiz.ui.theme.QuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -86,14 +97,31 @@ fun q(viewModel: QuizVeiwModel = androidx.lifecycle.viewmodel.compose.viewModel(
                 viewModel.onSubmit()
                 Text(text = ("SUBMIT"))
             }
+
           }
 
 
     }
-item{
-    Text(viewModel.score.value)//score
-}
-}
+        item{
+            Row(     modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+
+            Text(text ="Show Score")
+                //text = viewModel.score.value){
+            var checked by remember { mutableStateOf(true) }
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                     checked= it
+
+                }
+            )
+            if (checked){Text(text = viewModel.score.value)}
+
+        }}
+
+}}
 
 
-}
+
+
